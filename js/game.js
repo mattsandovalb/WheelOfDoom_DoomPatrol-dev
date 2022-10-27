b7f8669aee49d98643fe19e7439d3e90ced038ec
Elfin2 <eduinfo9696@gmail.com>
2022-10-27 11:58:03 +0200

    "Actualizacion"

--- js/game.js
+++ js/game.js
@@ -26,6 +26,21 @@ function killPlayer(){
     }
 }
 
+function popUPSacrificio(nombre){
+
+    const open = document.getElementById('open');
+    const modal_container = document.getElementById('modal_container');
+    const close = document.getElementById('close');
+
+    open.addEventListener('click', () => {
+        modal_container.classList.add('show');  
+    });
+
+    close.addEventListener('click', () => {
+        modal_container.classList.remove('show');
+    });
+
+}
 
 const buttonKill = document.getElementById('kill')
 
