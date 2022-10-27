3489809b3f2b6af0ca46e5e09c6b62b269027e84
Gary HL <103480668+GaryHL@users.noreply.github.com>
2022-10-27 09:17:44 +0200

    popup

--- js/game.js
+++ js/game.js
@@ -12,7 +12,10 @@ function killPlayer(){
 
         let indiceAleatorio = 0 + Math.floor(Math.random() * listaDeVivos.length); //generar un numero entero desde 0 hasta la longitud del array
         
-        console.log(listaDeVivos[indiceAleatorio]) //console log del jugador seleccionado
+        let nombreDelJugadorSeleccionado = listaDeVivos[indiceAleatorio].playerName;
+
+        console.log(nombreDelJugadorSeleccionado) //console log del jugador seleccionado
+        
 
         listaDeMuertos.push(listaDeVivos[indiceAleatorio]) // enviamos al jugador seleccionado a la lsita de muertos
 
@@ -20,9 +23,11 @@ function killPlayer(){
 
         console.log(listaDeVivos) // console.log de lista de vivos
 
+        soloKill(nombreDelJugadorSeleccionado)
+
     }
     else{ // si la condición anterior no se cumple entonces el array de vivos está vacio
-        alert("Ya no queda nadie vivo")
+        gameOver()
     }
 }
 
@@ -30,3 +35,74 @@ function killPlayer(){
 const buttonKill = document.getElementById('kill')
 
 buttonKill.addEventListener('click',killPlayer)
+
+
+
+//ONE CODER IS DEAD
+
+
+function soloKill (nameKilled){
+    
+    const modal_container = document.getElementById('modal_container');
+    const btnNextKill = document.getElementById('nextKill');
+    const alertPlayerDeleted = document.getElementById('alertPlayerDeleted');
+    // const open = document.getElementById('kill');
+
+    alertPlayerDeleted.innerHTML= `El jugador...${nameKilled} ha sido eliminado`;
+    
+    btnNextKill.addEventListener('click', () =>{
+        modal_container.classList.remove("show");
+    })
+
+    modal_container.classList.add("show");
+
+
+    // const close = document.getElementById('close');
+    
+    // open.addEventListener('click', () => {
+    //     document.getElementById('gameover').innerHTML = `Has matado ${nameKilled.playerName}`;
+    //     modal_container.classList.add('show');  
+    //   });
+    
+    //   close.addEventListener('click', () => {
+    //     modal_container.classList.remove('show');
+    //   });
+}
+
+function gameOver (){
+    
+    const modal_container = document.getElementById('modal_container');
+    const btnClose = document.getElementById('btnClose');
+
+    modal_container.innerHTML= `<button id="btnClose">close</button>`;
+    btnClose.addEventListener('click', () =>{
+        modal_container.classList.remove("show");
+    })
+
+    modal_container.classList.add("show");
+}
+
+
+
+
+
+
+//ALL CODERS DEAD POPUP
+// function gameOver (){
+    
+//     const open = document.getElementById('kill');
+//     const modal_container = document.getElementById('modal_container');
+//     const close = document.getElementById('close');
+    
+//     open.addEventListener('click', () => {
+//         document.getElementById('gameover').innerHTML = 'Game Over <br>All coders are  Dead'
+//         modal_container.classList.add('show');  
+//       });
+    
+//       close.addEventListener('click', () => {
+//         modal_container.classList.remove('show');
+//       });
+//     }
+    
+
+
