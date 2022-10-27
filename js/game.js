e922618a523fe52b13f9bd1c0c9374a59ba8b132
Gary HL <103480668+GaryHL@users.noreply.github.com>
2022-10-27 18:27:00 +0200

    add animations, and name in the player

--- js/game.js
+++ js/game.js
@@ -1,154 +1,150 @@
-
 let players = JSON.parse(localStorage.getItem("playersKey")); // Traemos el array de jugadores desde el local storage
 
 let listaDeVivos = players; // recibimos el array de jugadores y hacemos un clon
 
-let listaDeMuertos =[]
+let listaDeMuertos = [];
+
+let jugadorSacrificado = "";
 
 // selecciona un indice aleatorio dentro del array
 
-function killPlayer(){
-    if(listaDeVivos.length > 0){ //si la longitud del array es mayor a 0 entonces pasa lo siguiente
+function killPlayer() {
+  if (listaDeVivos.length > 0) {
+    //si la longitud del array es mayor a 0 entonces pasa lo siguiente
 
-        let indiceAleatorio = 0 + Math.floor(Math.random() * listaDeVivos.length); //generar un numero entero desde 0 hasta la longitud del array
-        
-        let nombreDelJugadorSeleccionado = listaDeVivos[indiceAleatorio].playerName;
+    let indiceAleatorio = 0 + Math.floor(Math.random() * listaDeVivos.length); //generar un numero entero desde 0 hasta la longitud del array
 
-        console.log(nombreDelJugadorSeleccionado); //console log del jugador seleccionado
-        
+    let nombreDelJugadorSeleccionado = listaDeVivos[indiceAleatorio].playerName;
 
-        listaDeMuertos.push(listaDeVivos[indiceAleatorio]); // enviamos al jugador seleccionado a la lsita de muertos
+    jugadorSacrificado = listaDeVivos[indiceAleatorio].playerName;
 
-        listaDeVivos.splice(indiceAleatorio,1); // eliminar al jugador seleccionado de la lista de vivos
+    console.log(nombreDelJugadorSeleccionado); //console log del jugador seleccionado
 
-        console.log(listaDeVivos); // console.log de lista de vivos
+    listaDeMuertos.push(listaDeVivos[indiceAleatorio]); // enviamos al jugador seleccionado a la lsita de muertos
 
-        //soloKill(nombreDelJugadorSeleccionado)
-        setTimeout(soloKill, 1700, (nombreDelJugadorSeleccionado));
-        comeKilled()
-        playGif()
-        setTimeout(esonder,900)
+    listaDeVivos.splice(indiceAleatorio, 1); // eliminar al jugador seleccionado de la lista de vivos
 
-    }
-    else{ // si la condición anterior no se cumple entonces el array de vivos está vacio
-        gameOver()
-    };
-    
-};
+    console.log(listaDeVivos); // console.log   de lista de vivos
 
+    //soloKill(nombreDelJugadorSeleccionado)
 
-const buttonKill = document.getElementById('kill');
+    // comeKilled()
+    // playGif()
+    // setTimeout(esonder,900)
+    return nombreDelJugadorSeleccionado;
+  } else {
+    // si la condición anterior no se cumple entonces el array de vivos está vacio
+    gameOver();
+  }
+}
 
-buttonKill.addEventListener('click',killPlayer);
+const buttonKill = document.getElementById("kill");
+buttonKill.addEventListener("click", animationAndPopUP);
 
+function animationAndPopUP() {
+  if (listaDeVivos.length >= 0) {
+    setTimeout(soloKill, 1700, jugadorSacrificado);
+    comeKilled();
+    playGif();
+    changeGiff(true);
+  } else {
+  }
+}
 
+const nextContainer = document.getElementById("nextcontainer");
+const nextButton = document.getElementById("nextButton");
+
+nextButton.addEventListener("click", nextPlayer);
+function nextPlayer() {
+  imgPlayer.classList.remove("shadow");
+  imgPlayer.classList.add("transleft");
+  nextContainer.classList.remove("block_next");
+  nextButton.classList.add("waitingNext");
+  imgPlayer.classList.remove("transdown");
+  let nombreDelJugadorSeleccionado = killPlayer();
+  changeGiff(false, nombreDelJugadorSeleccionado);
+}
 
 //ONE CODER IS DEAD
 
+function soloKill(nameKilled) {
+  const modal_container = document.getElementById("modal_container");
+  const btnNextKill = document.getElementById("nextKill");
+  const alertPlayerDeleted = document.getElementById("alertPlayerDeleted");
+  const btnList = document.getElementById("list");
 
-function soloKill (nameKilled){
-    
-    const modal_container = document.getElementById('modal_container');
-    const btnNextKill = document.getElementById('nextKill');
-    const alertPlayerDeleted = document.getElementById('alertPlayerDeleted');
-    const btnList = document.getElementById('list');
-    // const open = document.getElementById('kill');
-    btnList.innerHTML=("")
-    alertPlayerDeleted.innerHTML= `Player... ${nameKilled} has been deleted`;
-    
-    btnNextKill.addEventListener('click', () =>{
-        modal_container.classList.remove("show");
-        /*imgPlayer.classList.remove("transleft");*/
-        imgPlayer.classList.add("transleft");
-        imgPlayer.classList.remove("transdown");
-        setTimeout(document.getElementById("player").style.visibility = 'visible',3000);
+  // const open = document.getElementById('kill');
+  btnList.innerHTML = "";
+  alertPlayerDeleted.innerHTML = `Player... ${nameKilled} has been deleted`;
 
-    })
+  btnNextKill.addEventListener("click", () => {
+    modal_container.classList.remove("show");
+    /*imgPlayer.classList.remove("transleft");*/
+    imgPlayer.classList.add("shadow");
+    nextButton.classList.remove("waitingNext");
+    nextContainer.classList.add("block_next");
+  });
 
-
-    modal_container.classList.add("show");
-
-
-    // const close = document.getElementById('close');
-    
-    // open.addEventListener('click', () => {
-    //     document.getElementById('gameover').innerHTML = `Has matado ${nameKilled.playerName}`;
-    //     modal_container.classList.add('show');  
-    //   });
-    
-    //   close.addEventListener('click', () => {
-    //     modal_container.classList.remove('show');
-    //   });
+  modal_container.classList.add("show");
 }
-
-
-// function gameOver (){
-    
-//     
-//     const modal_container = document.getElementById('modal_container');
-//     const btnClose = document.getElementById('btnClose');
-
-//     modal_container.innerHTML= `<button id="btnClose">close</button>`;
-//     btnClose.addEventListener('click', () =>{
-//         modal_container.classList.remove("show");
-//     })
-
-//     modal_container.classList.add("show");
-// }
-
-
-
-
-
 
 //ALL CODERS DEAD POPUP
-function gameOver (){  
-    const removeButtonContinue = document.getElementById('nextKill');
-    const open = document.getElementById('kill');
-    const modal_container = document.getElementById('modal_container');
-    const close = document.getElementById('close'); 
-    const containerButtons = document.getElementById("container_buttons") 
+function gameOver() {
+  const removeButtonContinue = document.getElementById("nextKill");
+  const open = document.getElementById("kill");
+  const modal_container = document.getElementById("modal_container");
+  const close = document.getElementById("close");
+  const containerButtons = document.getElementById("container_buttons");
 
-    open.addEventListener('click', () => {
-        document.getElementById('alertPlayerDeleted').innerHTML = 'Game Over <br>All coders are  Dead'
-        removeButtonContinue.innerHTML= '';
-        modal_container.classList.add('show');  
-       });  
-    containerButtons.innerHTML=`<a href="./list.html" id="list">            
+  open.addEventListener("click", () => {
+    document.getElementById("alertPlayerDeleted").innerHTML =
+      "Game Over <br>All coders are  Dead";
+    removeButtonContinue.innerHTML = "";
+    modal_container.classList.add("show");
+  });
+  containerButtons.innerHTML = `<a href="./list.html" id="list">            
     <img src="../assets/svg/list.svg" alt="" class="btcontinue">
-  </a>`
-        // close.addEventListener('click', () => {
-        //   modal_container.classList.remove('show');
-        // });
-        
+  </a>`;
 }
-const imgPlayer = document.getElementById('player');
+const imgPlayer = document.getElementById("player");
 
-function comeKilled (){
-    imgPlayer.classList.remove("transleft")
-    imgPlayer.classList.add("transdown")
+function comeKilled() {
+  imgPlayer.classList.remove("transleft");
+  imgPlayer.classList.add("transdown");
 }
 
-const gunContainer = document.getElementById("gun_container")
-let shootSound = new Audio("../assets/sounds/Sounds/shootgun_shoot_1.mp3")
-let chargeSound = new Audio("../assets/sounds/Sounds/reload_1.mp3")
+const gunContainer = document.getElementById("gun_container");
+let shootSound = new Audio("../assets/sounds/Sounds/shootgun_shoot_1.mp3");
+let chargeSound = new Audio("../assets/sounds/Sounds/reload_1.mp3");
 
-function playGif(){
-    gunContainer.innerHTML=`<img src="../assets/img/gunGif.gif" alt="gun" class="gif_gun">`;
-    setTimeout(playimg, 1700);
-    shootSound.play();
-    setTimeout(chargePLay, 500);
+function playGif() {
+  gunContainer.innerHTML = `<img src="../assets/img/gunGif.gif" alt="gun" class="gif_gun">`;
+  setTimeout(playimg, 1700);
+  shootSound.play();
+  setTimeout(chargePLay, 500);
 }
 
 chargeSound.play();
 
-function chargePLay(){
-    chargeSound.play();
+function chargePLay() {
+  chargeSound.play();
 }
-function playimg(){
-    gunContainer.innerHTML=`<img src="../assets/img/maskgroup.png" alt="gun" class="img_gun">`;
+function playimg() {
+  gunContainer.innerHTML = `<img src="../assets/img/maskgroup.png" alt="gun" class="img_gun">`;
 }
 
-function esonder(){
-    document.getElementById("player").style.visibility = 'hidden';
+function esonder() {
+  document.getElementById("player").style.visibility = "hidden";
+}
+
+// funcion para cambiar de giffs del player
+
+const playerGif = document.getElementById("player");
+
+function changeGiff(instruction, nombre) {
+  if (instruction == true) {
+    playerGif.innerHTML = `<img class="player"  src="../assets/img/player-dead.gif" alt="player" id="player_img">`;
+  } else {
+    playerGif.innerHTML = `<img class="player"  src="../assets/img/walking-player.gif" alt="player" id="player_img"><h4>${nombre}</h4>`;
+  }
 }
